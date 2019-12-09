7e1ae2345ca25933df02ac98937e03f0439e985c
Kam <donggyu@ad.helsinki.fi>
2019-12-09 10:57:29 +0200

    exercise 6

--- data/meet_and_repeat.R
+++ data/meet_and_repeat.R
@@ -9,3 +9,29 @@ head(bprs,3)
 rats<-read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/rats.txt", sep="\t",header=TRUE)
 str(rats)
 summary(rats)
+
+#Conversion the categorial variables to factors
+bprs$treatment<-as.factor(bprs$treatment)
+bprs$subject<-as.factor(bprs$subject)
+str(bprs)
+head(bprs)
+
+rats$ID<-as.factor(rats$ID)
+rats$Group<-as.factor(rats$Group)
+str(rats)
+
+#Convert the datasets to long form
+library(dplyr)
+library(tidyr)
+bprs<-bprs%>%gather(key=week, value=bprs_score, -treatment, -subject)
+bprs
+str(bprs)
+rats<-rats%>%gather(key=time, value=gram, -Group, -ID)
+rats
+str(rats)
+
+
+setwd("Z:\\IODS-project\\data")
+
+write.csv(rats,file="rats.csv",row.names = FALSE)
+write.csv(bprs,file="bprs.csv",row.names = FALSE)
