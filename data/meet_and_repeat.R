b8cd0ddfc1582e4785a280d07de0410da23c755a
Kam <donggyu@ad.helsinki.fi>
2019-12-09 10:57:08 +0200

    exercise 6

--- data/meet_and_repeat.R
+++ data/meet_and_repeat.R
@@ -1,1 +1,11 @@
 #Donggyu Kam, 08.12.2019
+
+#Read table
+bprs<-read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/BPRS.txt", sep="", header=T)
+str(bprs)
+summary(bprs)
+head(bprs,3)
+
+rats<-read.table("https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/rats.txt", sep="\t",header=TRUE)
+str(rats)
+summary(rats)
